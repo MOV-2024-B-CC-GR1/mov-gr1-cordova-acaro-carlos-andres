def6013addac7f49782e99a9270af7ee84e3e6a3
Carlos Córdova <108033929+CarlosCordovaEPN@users.noreply.github.com>
2024-12-09 16:29:29 -0500

    Android y kotlin

---
+++ 01-Android/app/src/main/java/com/example/ccgr12024b_caca/BBaseDatosMemoria.kt
@@ -0,0 +1,12 @@
+package com.example.ccgr12024b_caca
+
+class BBaseDatosMemoria {
+    companion object{
+        val arregloBEntrenador = arrayListOf<BEntrenador>()
+        init {
+            arregloBEntrenador.add(BEntrenador(1,"Carlos","c@c.com"))
+            arregloBEntrenador.add(BEntrenador(2,"Andrés","c@c.com"))
+            arregloBEntrenador.add(BEntrenador(3,"Córdova","c@c.com"))
+        }
+    }
+}
